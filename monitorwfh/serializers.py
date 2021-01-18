b42cfe818d481f980673eb6fab0e81740b22cc4a
Teguh Prasetyo <teguhpraz715@gmail.com>
2021-01-18 09:45:54 +0700

    Tambah serialzer

--- monitorwfh/serializers.py
+++ monitorwfh/serializers.py
@@ -1,4 +1,4 @@
-from .models import Jabatan, Divisi
+from .models import Jabatan, Divisi, Karyawan, Absensi, ListJob
 from rest_framework import serializers
 
 class JabatanSerializer(serializers.ModelSerializer):
@@ -11,3 +11,18 @@ class DivisiSerializer(serializers.ModelSerializer):
     class Meta:
         model = Divisi
         fields = ['id', 'nama_divisi']
+
+class KaryawanSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Karyawan
+        fields = ['nik', 'nama_karyawan', 'alamat', 'jk', 'agama', 'tlp', 'jabatan', 'divisi']
+
+class AbsensiSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Absensi
+        fields = ['id', 'nik', 'tanggal', 'masuk', 'pulang', 'status_absen']
+
+class ListJobSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ListJob
+        fields = ['id', 'nama_job', 'deadline', 'status', 'nik']
